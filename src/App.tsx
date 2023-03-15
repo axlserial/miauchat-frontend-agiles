5e817208a7c36812dc9e86bbb2c65922ed0b8c7e
Maggy30ev <maggy30ev@gmail.com>
2023-03-15 01:08:46 -0600

    Login interface sin funcionalidad y favaicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Rutas } from "./routes";
+import Login from "./pages/Login";
 
 // Page components
 import NotFound from "./pages/NotFound";
+import { Container } from "@mantine/core";
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<BrowserRouter >
 			<Routes>
 				{/* Ruta raíz */}
 				<Route index element={<Navigate to={Rutas.login} />} />
 
 				{/* Ruta login */}
-				<Route path={Rutas.login} element={<div>Compontente Login</div>} />
+				<Route path={Rutas.login} element={<div style={{ backgroundImage: "url(src/assets/images/background_light_miauChat.svg)", width: "auto", height: "98vh" }}> <Container sx={{ paddingTop: "4%", paddingBottom: "4%" }}><Login /></Container> </div>} />
 
 				{/* Ruta registro */}
 				<Route path={Rutas.signup} element={<div>Compontente Registro</div>} />
---
+++ src/pages/Login.tsx
@@ -0,0 +1,73 @@
+import {
+  TextInput,
+  Image,
+  Stack,
+  PasswordInput,
+  Paper,
+  Title,
+  Container,
+  Button,
+} from "@mantine/core";
+import { IconEyeCheck, IconEyeOff } from '@tabler/icons-react';
+
+const Login = () => {
+  return (
+    <div >
+      <Container >
+        <Paper shadow="sm" radius="sm" p="x1" withBorder>
+          <Stack
+            align="center"
+            justify="space-around"
+            sx={{ padding: "90px 15px 0px 15px" }}
+          >
+            <Image width={200} height={200} fit="contain" src="src/assets/images/Icon_MiauChat.svg" />
+            <Container>
+              <Title
+                sx={(theme) => ({
+                  fontFamily: `Open Sans, ${theme.fontFamily}`,
+                  fontWeight: 600,
+                  fontSize: "25px",
+                  paddingTop: "30px",
+                  paddingBottom: "50px",
+                })}>
+                ¡Bienvenido a MiauChat!
+              </Title>
+            </Container>
+          </Stack>
+          <form action="">
+            <Container size={500} sx={{ padding: "0px 15px 90px 15px" }}>
+              <Stack
+                spacing="xl"
+              >
+                <TextInput
+                  name="user"
+                  label="Usuario"
+                  placeholder="michiUsuario06"
+                  size="md"
+                  required
+
+                />
+                <PasswordInput
+                  label="Contraseña"
+                  placeholder="*****"
+                  size="md"
+                  visibilityToggleIcon={({ reveal, size }) =>
+                    reveal ? <IconEyeOff size={size} /> : <IconEyeCheck size={size} />
+                  }
+                  required
+                />
+
+                <Button variant="gradient" gradient={{ from: 'cyan', to: '#26D6F1', deg: 35 }} size="md" >
+                  Ingresar
+                </Button>
+
+              </Stack>
+            </Container>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
