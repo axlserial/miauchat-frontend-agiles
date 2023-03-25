07b0d0248281c5231ff087990d30b8ca5c711753
Axel García <axldorian3@gmail.com>
2023-03-25 17:28:24 -0600

    Update comments routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,7 @@
-/*
- * Una constante con las rutas de la aplicación.
- */
-
+// Constante con la ruta base de la aplicación.
 export const base = "/miauchat/";
 
+// Un objeto con las rutas de la aplicación.
 export const Rutas = {
 	login: base + "iniciar-sesion",
 	signup: base + "registrarse",
