f0ed4e3bf8ba671a6f8dde088ad9147c5414a71b
Marcos Santiago <onligaf@outlook.es>
2023-03-25 18:25:27 -0600

    corrección del path - pantalla principal

--- src/pages/PantallaPrincipal.tsx
+++ src/pages/PantallaPrincipal.tsx
@@ -7,12 +7,12 @@ import {
 	Grid,
 	Paper,
 	Box,
-	Avatar, 
+	Avatar,
 	useMantineTheme,
 	TextInput, Modal
 } from "@mantine/core";
 import { useEventListener, useScrollIntoView, useDisclosure } from "@mantine/hooks";
-import React, { useState,useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { ModalCrearSala } from '../pages/crearModal';
 import { useSalaStore } from "../stores/salaStore";
@@ -23,14 +23,18 @@ import { logout } from "../services/usuarios";
 
 // Forma de importar una imagen
 import bgImage from "../assets/images/Icon_MiauChat.svg";
-const usuario = useSessionStore((state) => state.usuario);
-const rutaImagen = "/src/assets/images/Users_profile/" + getNombreImagen(usuario.foto_perfil);
+import img1 from "../assets/images/Users_profile/Gato_caja.png";
+import img2 from "../assets/images/Users_profile/Gato_flor.png";
+import img3 from "../assets/images/Users_profile/Gato_gorro.png";
+import img4 from "../assets/images/Users_profile/Gato_maceta.png";
+import img5 from "../assets/images/Users_profile/Gato_mause.png";
+import img6 from "../assets/images/Users_profile/Gato_pez.png";
 
 const floatingButtonStyle = {
 	bottom: '20px',
 	right: '20px',
 	zIndex: 1000, // Asegura que el botón esté por encima de otros elementos en la página
-  };
+};
 
 
 function PantallaPrincipal() {
@@ -45,19 +49,34 @@ function PantallaPrincipal() {
 	const theme = useMantineTheme();
 	const clearUsuario = useSessionStore((state) => state.clearUsuario);
 	const { salas, fetchSalas } = useSalaStore();
+	const usuario = useSessionStore((state) => state.usuario);
+	let rutaImagen = null;
+	if (usuario.foto_perfil == 1)
+		rutaImagen = img1
+	if (usuario.foto_perfil == 2)
+		rutaImagen = img2
+	if (usuario.foto_perfil == 3)
+		rutaImagen = img3
+	if (usuario.foto_perfil == 4)
+		rutaImagen = img4
+	if (usuario.foto_perfil == 5)
+		rutaImagen = img5
+	if (usuario.foto_perfil == 6)
+		rutaImagen = img6
+
+
 	useEffect(() => {
 		(async () => await fetchSalas(usuario.id))()
-	  }, []);
-	console.log(salas);
-	{/*salida de la app*/}
+	}, []);
+	{/*salida de la app*/ }
 	useEffect(() => {
-		if (exit==1) {
+		if (exit == 1) {
 			logout().then(() => {
 				clearUsuario();
 				navigate(Rutas.login);
 			});
 		}
-	},[exit])
+	}, [exit])
 	const listItems = salas.reverse().map((sala) => (
 		<Box pt={1} pb={1} key={sala.id}>
 			<Paper
@@ -98,11 +117,12 @@ function PantallaPrincipal() {
 											marginTop: "5%"
 										}}
 									>
+										{/*foto de perfil */}
 										<Avatar
-											src= {rutaImagen}
+											src={rutaImagen}
 											radius="90%"
 											size="30%"
-										/>
+										/>;
 									</div>
 								</Grid.Col>
 								<Grid.Col span={2}>
@@ -146,13 +166,13 @@ function PantallaPrincipal() {
 						</Paper>
 					</Group>
 					<div style={floatingButtonStyle}>
-						<ButtonMenu/>
+						<ButtonMenu />
 					</div>
-					
+
 				</div>
-				
+
 				{/*parte derecha*/}
-				
+
 				<div style={{ width: "70%", float: "right" }}>
 					<div
 						style={{
@@ -173,49 +193,40 @@ function PantallaPrincipal() {
 				</div>
 			</Card>
 		</div>
-		
+
 	);
 	function ButtonMenu() {
 		const theme = useMantineTheme();
 		const [opened, { open, close }] = useDisclosure(false);
 		return (
 			<>
-		<Modal
-        opened={opened}
-        onClose={close}
-        title="Crear/Unirse a Sala"
-        overlayProps={{
-          color: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
-          opacity: 0.55,
-          blur: 3,
-        }}
-      >
-        {/* Modal content */
-			<ModalCrearSala/>
-		}
-      </Modal>
+				<Modal
+					opened={opened}
+					onClose={close}
+					title="Crear/Unirse a Sala"
+					overlayProps={{
+						color: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
+						opacity: 0.55,
+						blur: 3,
+					}}
+				>
+					{/* Modal content */
+						<ModalCrearSala />
+					}
+				</Modal>
 
-      <Group position="right">
-		<div>
-		<Button rightIcon={<IconPlus size="3rem" stroke={4} />} pr={12} variant="outline" onClick={open}></Button>
-		</div>
-        
-      </Group>
-    </>
-  );
-	}	
+				<Group position="right">
+					<div>
+						<Button rightIcon={<IconPlus size="3rem" stroke={4} />} pr={12} variant="outline" onClick={open}></Button>
+					</div>
+
+				</Group>
+			</>
+		);
+	}
 }
 
-function getNombreImagen(numero: number) {
-	const nombreImagen: {[numero: number]:any} = {
-		1: "Gato_caja.png",
-		2: "Gato_flor.png",
-		3: "Gato_gorro.png",
-		4: "Gato_maceta.png",
-		5: "Gato_mause.png",
-		6: "Gato_pez.png",
-	  };
-	return nombreImagen[numero];
-  }
-  
+
+
+
 export default PantallaPrincipal;
