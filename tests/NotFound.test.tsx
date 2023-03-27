cac65db72bdb870dd10ffa7b3ed64e279d7d3727
Maggy30ev <maggy30ev@gmail.com>
2023-03-26 18:40:11 -0600

    Pruebas de la página NotFound

--- tests/NotFound.test.tsx
+++ tests/NotFound.test.tsx
@@ -1,16 +1,47 @@
 import React from "react";
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
 import NotFound from "../src/pages/NotFound";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Rutas } from "../src/routes";
 
 // Descripción del componente
 describe("NotFound.tsx", () => {
-	// Test 1
-	test("Se espera que haya un texto: 404 - Not Found", () => {
-		// Renderizar el componente
-		render(<NotFound />);
 
-		// Buscar el texto
-		expect(screen.getByText("404 - Not Found")).toBeDefined();
+	beforeEach(() => {
+		// Renderizar el componente
+		render(<Router> <NotFound /> </Router>);
 	});
+
+	// Test 1
+	test("Se espera que haya una imagen", () => {
+		// Buscar la imagen
+		expect(screen.getByRole("img")).toBeDefined();
+	});
+
+	// Test 2
+	test("Se espera que haya un texto: Ruta no encontrada", () => {
+		// Buscar el texto
+		expect(screen.getByText("Ruta no encontrada")).toBeDefined();
+	});
+
+	// Test 3
+	test("Se espera que haya un botón: Regresar a la página principal", () => {
+		// Buscar el botón
+		expect(screen.getByText("Regresar a la página principal")).toBeDefined();
+	});
+
+	// Test 4
+	test("Se espera que el botón al hacer click nos rediriga a la ruta principal de los chats", () => {
+		// Buscar el botón
+		const button = screen.getByRole("button");
+
+		// Hacer click en el botón
+		fireEvent.click(button);
+
+		// Navegar a la ruta principal
+		expect(window.location.pathname).toContain(Rutas.chats);
+
+	});
+
 });
